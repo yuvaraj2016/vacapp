96daaba31840d3042aaa371acabd2e11b11493ab
yuvaraj2016 <juvaraj2016@gmail.com>
2020-12-11 12:38:26 -0800

    updates

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,17 +1,17 @@
 <?php
 
 //local DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','vacapp');
+// define('DB_HOST','localhost');
+// define('DB_USER','root');
+// define('DB_PASS','');
+// define('DB_NAME','vacapp');
 
 //prod db
-//define('DB_HOST','sql154.main-hosting.eu.');
-//define('DB_USER','u777403328_hridh');
-//define('DB_PASS','14!salem');
-//define('DB_NAME','u777403328_hts');
-//
+define('DB_HOST','sql154.main-hosting.eu.');
+define('DB_USER','u968130738_vacapp	');
+define('DB_PASS','14!Salem');
+define('DB_NAME','u968130738_vacapp	');
+
 //// Establish database connection.
 try {
     $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'","SET GLOBAL time_zone = 'Asia/Calcutta'"));
