a662c3ac7982eea5dbc76e464760954174dc791f
yuvaraj2016 <juvaraj2016@gmail.com>
2020-12-12 15:09:01 -0800

    sfsafd

--- admin/appointment-list.php
+++ admin/appointment-list.php
@@ -87,7 +87,7 @@ if (isset($_POST['submit'])) {
 				Once VACCINE APPOINTMENT accepts the appointment you will get an email notification.
 				</p>';
 
-				$message .= '<a style="color:#fff;height:50px!important; margin-left:200px;margin-top:50px;font-size:18px; font-weight:bold; background-color:#17D4F4;padding:10px!important;padding-bottom:13px!important;text-align:center; text-decoration:none!important;" href="http://vacapp.hridhamtech.com/appointment-scheduler.php">
+				$message .= '<a style="color:#fff;height:50px!important; margin-left:200px;margin-top:50px;font-size:18px; vertical-align:middle!important;font-weight:bold; background-color:#17D4F4;padding:15px!important;padding-bottom:13px!important;text-align:center; text-decoration:none!important;" href="http://vacapp.hridhamtech.com/appointment-scheduler.php">
 				Click here to book your app</a>';
 				$message .= '</body></html>';
 				
